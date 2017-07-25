550da9c0d92a44e3a909c38b097725b8383f9de8
liznomura <lizn@hawaii.edu>
2017-07-24 16:14:42 -1000

    step 4

--- joins.sql
+++ joins.sql
@@ -5,6 +5,10 @@
 -- FROM posts
 -- WHERE users_id = 100;
 
-SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name, u.last_name
+-- SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name, u.last_name
+-- FROM posts p, users u
+-- WHERE u.id = p.users_id AND u.id = 200;
+
+SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.username
 FROM posts p, users u
-WHERE u.id = p.users_id AND u.id = 200;
+WHERE u.first_name = 'Norene' AND u.last_name = 'Schmitt';
