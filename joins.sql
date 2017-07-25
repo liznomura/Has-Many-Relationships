b0c6963343488f517541d04e781c2289c5801d2b
liznomura <lizn@hawaii.edu>
2017-07-24 16:13:07 -1000

    step 3

--- joins.sql
+++ joins.sql
@@ -1,6 +1,10 @@
 -- SELECT *
 -- FROM users;
 
-SELECT *
-FROM posts
-WHERE users_id = 100;
+-- SELECT *
+-- FROM posts
+-- WHERE users_id = 100;
+
+SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name, u.last_name
+FROM posts p, users u
+WHERE u.id = p.users_id AND u.id = 200;
