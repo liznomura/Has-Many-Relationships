b6e91b838d9681e9e2944c769d6084126808edb3
liznomura <lizn@hawaii.edu>
2017-07-24 16:24:11 -1000

    step 5

--- joins.sql
+++ joins.sql
@@ -9,6 +9,11 @@
 -- FROM posts p, users u
 -- WHERE u.id = p.users_id AND u.id = 200;
 
-SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.username
-FROM posts p, users u
-WHERE u.first_name = 'Norene' AND u.last_name = 'Schmitt';
+-- SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.username
+-- FROM posts p, users u
+-- WHERE u.first_name = 'Norene' AND u.last_name = 'Schmitt';
+
+SELECT u.username, p.created_at
+FROM users u LEFT JOIN posts p ON u.id = p.id
+WHERE DATE(p.created_at) >=  DATE('2015-01-01')
+ORDER BY p.created_at;
